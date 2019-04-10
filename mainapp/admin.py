361219a37f5a9d8097f29511f5adb0382ebd93d6
Oleg <slepcov_oleg@mail.ru>
2019-04-10 08:13:23 +0700

    Добавлены модели в админку

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from mainapp.models import ProductCategory, Product, ProductBySize, ProductImage
+
 
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(ProductBySize)
+admin.site.register(ProductImage)
